82b71892dd85357936a85de0c15d97d87aadc117
Solenov <colenov@yandex.ru>
2022-01-30 21:09:30 +0300

    Add doctest to example.py

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,3 +1,15 @@
-# Эта функция переворачивает переданную строку
 def reverse(str):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
     return str[::-1]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
